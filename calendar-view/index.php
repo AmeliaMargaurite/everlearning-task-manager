9a341ea3b86ec30b4133a7cc62b35b83da4fa9ce
AmeliaMargaurite <53086735+AmeliaMargaurite@users.noreply.github.com>
2022-08-30 14:21:27 +0200

    fix: functions file not imported correctly

--- calendar-view/index.php
+++ calendar-view/index.php
@@ -36,7 +36,7 @@ $calendar = new Calendar();
     <!-- Have ability to move back and forward through the months -->
     <?= $calendar->show() ?>
   </div>
-  <script src="./functions.js"></script>
+  <script src="./functions__1661861941141__.js"></script>
   <?php
   include_once(PAGE_END);
   ?>
